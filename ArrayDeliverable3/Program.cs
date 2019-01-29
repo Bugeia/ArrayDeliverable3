3760c255f2db48226ab1957a20af1f0c24e0ddd9
bugei <yanjunyang@mail.usf.edu>
2019-01-28 20:15:18 -0500

    array version 2

--- ArrayDeliverable3/Program.cs
+++ ArrayDeliverable3/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Author: Yanjun Yang
+// Date: 1/28/2019
+//Comments: This C# Console application code demostrates the use of arrays.
+using System;
 
 namespace ArrayDeliverable3
 {
@@ -6,7 +9,13 @@ namespace ArrayDeliverable3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine("Element Value=" + i + " ");
+            }
+            Console.ReadKey(true);
         }
     }
 }
